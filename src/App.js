89b7da083a5c1018a1ace9df6fc88a166709c97d
R <ryansereno@protonmail.com>
2022-10-17 10:51:26 -0400

    reorganized directories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import React from "react";
 import palmLogo from "./assets/condo icon.svg";
-import Kanban from "./components/kanban/Kanban";
 import DndComponent from "./components/kanban/DndComponent";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
@@ -13,9 +12,6 @@ function App() {
         <img src={palmLogo} alt="" height={150} />
       </header>
       <DndComponent />
-
-      {/*<Kanban />
-       */}
     </DndProvider>
   );
 }
